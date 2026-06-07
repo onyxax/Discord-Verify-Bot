05285f0f5e83c70bf1ec71b2c40b3861826ef240
onyxax <onyxax0@gmail.com>
2026-06-07 11:29:45 +0100

    Add bot activity status: Playing X servers

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, ActivityType } from 'discord.js';
 import { handleGuildCreate } from './events/guildCreate.js';
 import { handleInteractionCreate } from './events/interactionCreate.js';
 import { handleGuildMemberAdd } from './events/guildMemberAdd.js';
@@ -12,11 +12,18 @@ const client = new Client({
 });
 
 client.once('clientReady', () => {
+  const count = client.guilds.cache.size;
+  client.user.setActivity(`${count} servers`, { type: ActivityType.Playing });
   console.log(`[Verify Hydra] Bot online: ${client.user.tag}`);
-  console.log(`[Verify Hydra] Serving ${client.guilds.cache.size} guild(s)`);
+  console.log(`[Verify Hydra] Serving ${count} guild(s)`);
 });
 
-client.on('guildCreate', (guild) => handleGuildCreate(client, guild));
+client.on('guildCreate', (guild) => {
+  handleGuildCreate(client, guild);
+  const count = client.guilds.cache.size;
+  client.user.setActivity(`${count} servers`, { type: ActivityType.Playing });
+});
+
 client.on('guildMemberAdd', (member) => handleGuildMemberAdd(client, member));
 client.on('interactionCreate', (interaction) => handleInteractionCreate(client, interaction));
 
